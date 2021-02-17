bb4da8955ff3d9be1b791fa47deaaf8c796b7ce7
Sanders <seansanders@users.noreply.github.com>
2021-02-17 10:19:35 -0500

    Still working on generating ID data and performing Bi_Rank algorithm

--- R-code/TF_IDF_Example_BigramsV3.R
+++ R-code/TF_IDF_Example_BigramsV3.R
@@ -5,7 +5,7 @@ library(gofastr)
 library(tidyverse)
 library(birankr)
 
-path2="~/Projects/ProjectDrDuttaPersonal/Anchal/name_token_data.csv"
+path2="/media/sean/easystore/ProjectDrDuttaPersonal/Anchal/name_token_data.csv"
 x = read.csv(path2, header = TRUE)
 names(x) = c("num","doc_id","text")
 data_corpus2 = VCorpus(DataframeSource(x))
@@ -27,29 +27,59 @@ tdmSparseTermsRemoved
 #ap_td %>%cast_dfm(term, document, count)
 
 inspect(tdmSparseTermsRemoved[1:27,1:36501])
-setwd("~/Projects/ProjectDrDuttaPersonal/R_Scripts")
+setwd("/media/sean/easystore/ProjectDrDuttaPersonal/R_Scripts")
 write.table(inspect(tdmSparseTermsRemoved[1:27,1:36501]), file="mymatrix.txt")
 
-#Perform Birank
-df=inspect(tdmSparseTermsRemoved[1:27,1:36501])
-#df = as.data.frame(df, row.names=TRUE)
-#Extract the names
-rownames(df[1:10,1:10])
-colnames(df[1:10,1:10])
+#READ IN TABLE
+setwd("/media/sean/easystore/ProjectDrDuttaPersonal/R_Scripts")
+file="mymatrix.txt"
+tbl_names=read.table(file, nrows=1, header = FALSE, sep = " ", dec = ".")
+tbl=read.table(file, skip=1, header = FALSE, sep = " ", dec = ".")
 
-df[,"ann arbor"]
-for (name in colnames(df[1:10,1:10])){
-  for (val in df[,name]){
-    if(val != 0) {
-      cat(val,": T ")
-    }else{
-        cat(val," F ")
-      }
+lst_names=as.list(tbl_names)
+lst_names=prepend(lst_names,"N-Gram")
+colnames(tbl)=c(lst_names)
+
+write.table(tbl, file="mymatrixfinal.txt")
+
+#Now we need to create ID's
+df_tbl=as.data.frame(tbl)
+
+df_tbl[,-1] = (df_tbl[,-1] != 0)*1
+df_tbl
+lst_colnames=rownames(df_tbl)
+counter = 0
+lst_rownames=list()
+for (i in colnames(df_tbl)){
+  if(i != "N-Gram"){
+    counter = counter + 1
+    print(df_tbl[[i]])
+    rowname="V"+as.String(lst_colnames[counter])
+    assign(rowname,df_tbl[[i]])
+    lst_rownames[counter]<-rowname
   }
-  #print(df[,name])
+}
+rm(tbl, tbl_names, i, lst_colnames, counter)
+lst_names[1]<- NULL
+for (rowname in lst_rownames){
+  #print("ROW: "+as.String(rowname))
+  counter=0
+  for (val in get(eval(rowname))){
+    counter=counter+1
+    if(val == 1){
+      print(as.String(lst_names[counter])+":"+as.String(rowname))
+    }
+  }
 }
 
-#df[1:10,1][1:10]
+
+#Perform Birank
+#df=inspect(tdmSparseTermsRemoved[1:27,1:36501])
+
+#rownames(df[1:10,1:10])
+#colnames(df[1:10,1:10])
+
+#TEST
 #Extract the ngram names
 br_birank(df)
 df2 <- data.table(
